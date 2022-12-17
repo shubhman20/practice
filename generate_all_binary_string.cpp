28bb1b7b2056fc0106b89b8235d7367faa985918
Manish Kumar Sharma <105535356+shubhman20@users.noreply.github.com>
2022-12-17 13:31:18 +0530

    Topic - recursion

---
+++ generate_all_binary_string.cpp
@@ -0,0 +1,90 @@
+/*QUESTION:- 
+Given an integer N , generate all binary strings of size N which do not contain consecutive 1s.
+
+A binary string is that string which contains only 0 and 1.
+
+Example 1:
+
+Input:
+N = 3
+Output:
+000 , 001 , 010 , 100 , 101
+Explanation:
+None of the above strings contain consecutive 1s. "110" is not an answer as it has '1's occuring consecutively.
+
+*/
+
+
+
+
+//{ Driver Code Starts
+//Initial Template for C++
+#include<bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+//User function Template for C++
+
+class Solution{
+public:
+    void create(string &a,int num,vector<string>&ans){
+        if(num<=0){
+            ans.push_back(a);
+            return;
+        }
+        if(a[a.size()-1]=='1'){
+            a.push_back('0');
+            create(a,num-1,ans);
+            a.pop_back();
+        }
+        else{
+            a.push_back('0');
+            create(a,num-1,ans);
+            a.pop_back();
+            a.push_back('1');
+            create(a,num-1,ans);
+            a.pop_back();
+        }
+    }
+
+
+    void generateBinaryStrings(int& num){
+        //Write your code
+        vector<string>ans;
+        string a="0";
+        string b="1";
+        create(a,num-1,ans);
+        create(b,num-1,ans);
+        sort(ans.begin(),ans.end());
+        for(int i=0;i<ans.size();i++){
+            cout<<ans[i]<<" ";
+        }
+        
+    }
+};
+
+//{ Driver Code Starts.
+int main(){
+    int t = 1;
+    cin >> t;
+
+    // freopen ("output_gfg.txt", "w", stdout);
+
+    while(t--){
+        //Input
+        int n; cin >> n;
+
+        Solution obj;
+        // cout << setprecision(9) << obj.switchCase(choice,vec) << endl;
+        obj.generateBinaryStrings(n);
+        cout << endl;
+        
+        // cout << "~\n";
+    }
+    // fclose(stdout);
+
+    return 0;
+}
+
+// } Driver Code Ends
