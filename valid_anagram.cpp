097eedf6bcc6fc092f5ad7b968686a48518b2204
Manish Kumar Sharma <105535356+shubhman20@users.noreply.github.com>
2022-12-30 00:48:16 +0530

    String

---
+++ valid_anagram.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        if(s.size()!=t.size()) return false;
+        unordered_map<char,int>m1,m2;
+        for(int i=0;i<s.size();i++){
+            m1[s[i]]++;
+            m2[t[i]]++;
+        }
+        for(int i=0;i<s.size();i++){
+            if(m1[s[i]]!=m2[s[i]])
+                return false;
+        }
+        return true;
+    }
+};
