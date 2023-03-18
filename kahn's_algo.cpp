7cb3b2bb79a60dfa11fce8ab8999212fda252aa5
Manish Kumar Sharma <105535356+shubhman20@users.noreply.github.com>
2023-03-18 19:26:29 +0530

    Graph

---
+++ kahn's_algo.cpp
@@ -0,0 +1,33 @@
+class Solution
+{
+	public:
+	vector<int> topoSort(int V, vector<int> adj[]) 
+	{
+	    // code here
+	   vector<int> indegree(V,0);
+	   for(int i=0;i<V;i++){
+	       for(auto &it :adj[i]){
+	           indegree[it]++;
+	       }
+	   }
+	   queue<int> q;
+	   for(int i=0;i<V;i++){
+	       if(indegree[i]==0){
+	           q.push(i);
+	       }
+	   }
+	   vector<int> ans;
+	   while(!q.empty()){
+	       ans.push_back(q.front());
+	       int node = q.front();
+	       q.pop();
+	       for(auto &it : adj[node]){
+	           indegree[it]--;
+	           if(indegree[it]==0){
+	               q.push(it);
+	           }
+	       }
+	   }
+	   return ans;
+	}
+};
