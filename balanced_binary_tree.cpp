3ad4b14dc4763c41ec2ccc4c0596d2c7f5c3fede
Manish Kumar Sharma <105535356+shubhman20@users.noreply.github.com>
2023-04-06 02:45:12 +0530

    Binary Tree

---
+++ balanced_binary_tree.cpp
@@ -0,0 +1,16 @@
+
+class Solution {
+public:
+    int height(TreeNode* root){
+        if(root==NULL) return 0;
+        return max(height(root->left),height(root->right))+1;
+    }
+    bool isBalanced(TreeNode* root) {
+        if(root==NULL) return true;
+        if(abs(height(root->left)-height(root->right))>1){
+            return false;
+        }
+        return isBalanced(root->left)&&isBalanced(root->right);
+
+    }
+};
