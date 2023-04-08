7a51054b0a524d34eed8e64bba7cf6e20644dae3
Manish Kumar Sharma <105535356+shubhman20@users.noreply.github.com>
2023-04-08 17:44:25 +0530

    Array

---
+++ 2sum.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        map<int,int> mp;
+        vector<int> ans;
+        for(int i=0;i<nums.size();i++){
+            int val=target - nums[i];
+            if(mp.find(val)!=mp.end()){
+                ans.push_back(mp[val]);
+                ans.push_back(i);
+                return ans;
+            }
+            if(mp.find(nums[i])==mp.end()){
+                mp[nums[i]]=i;
+            }
+        }
+        return ans;
+    }
+};
