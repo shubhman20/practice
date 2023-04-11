a8eaf5297ae2ab79e95f844466bcc0c699d0575e
Manish Kumar Sharma <105535356+shubhman20@users.noreply.github.com>
2023-04-11 18:25:55 +0530

    Array

---
+++ set_matrix_zero.cpp
@@ -0,0 +1,42 @@
+class Solution {
+public:
+    void setZeroes(vector<vector<int>>& matrix) {
+        int col0=0;
+        int m = matrix.size();
+        int n = matrix[0].size();
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(matrix[i][j]==0){
+                    matrix[i][0]=0;
+                    if(j!=0){
+                        matrix[0][j]=0;
+                    }
+                    else{
+                        col0=1;
+                    }
+                }
+            }
+        }
+
+        //traversing to make all values zero which has either row or column as 0
+        for(int i=1;i<m;i++){
+            for(int j=1;j<n;j++){
+                if(matrix[i][0]==0||matrix[0][j]==0){
+                    matrix[i][j]=0;
+                }
+            }
+        }
+
+        if(matrix[0][0]==0){
+            for(int i=0;i<n;i++){
+                matrix[0][i]=0;
+            }
+        }
+
+        if(col0==1){
+            for(int i=0;i<m;i++){
+                matrix[i][0]=0;
+            }
+        }
+    }
+};
